e90fa8eba033a77af0f50ad3ae8f5bbb4ff601dc
nikheel indanoor <nikheelindanoor123@gmail.com>
2023-12-09 22:43:08 +0530

    Added company login page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,8 @@ import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { CouponContextProvider } from "./context/CouponContext.jsx";
-
 import { AuthContextProvider } from "./context/AuthContext.jsx";
+import CompanyLogin from "./pages/CompanyLogin.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <AuthContextProvider>
@@ -12,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <Router>
                 <Routes>
                     <Route path="/" element={<App />} />
+                    <Route path="/companyLogin" element={<CompanyLogin />} />
                 </Routes>
             </Router>
         </CouponContextProvider>
