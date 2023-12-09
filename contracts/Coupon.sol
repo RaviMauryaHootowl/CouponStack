816c7db680499422af3484a4ba43d57f02cb1c08
nikheel indanoor <nikheelindanoor123@gmail.com>
2023-12-09 17:20:43 +0530

    Basic Contract

---
+++ contracts/Coupon.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+
+contract Coupon is ERC1155, ERC1155Burnable {
+    address private owner;
+
+    constructor() ERC1155("") {
+        owner = msg.sender;
+    }
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private companyCouponIds;
+
+    mapping(uint256 => string[]) id_to_ipfsUri;
+    mapping(uint256 => address) private _tokenOwners;
+    mapping(address => uint256) private _ownedTokensCount;
+    mapping(uint256 => string) private tokenToUri;
+    mapping(uint256 => uint256) private couponIdToCompanyId;
+
+    struct Company {
+        uint256 companyId;
+        string name;
+        string url;
+        address companyAddress;
+        uint256 count;
+    }
+
+    struct CompanyCoupon {
+        uint256 companyId;
+        uint256 couponId; //TokenId
+        address owner;
+        bool isPurchased;
+        string cid; //TokenURI
+        string category;
+        uint rating;
+    }
+
+    uint256 private CompanyCount;
+    mapping(uint256 => CompanyCoupon) public companyCouponMapping;
+    mapping(address => Company) public addressToCompany;
+    mapping(uint256 => Company) private companyMapping;
+    mapping(uint256 => uint256) private companyIdToCouponsCount;
+
+    function addCompany(
+        string memory name,
+        string memory url,
+        address caddress
+    ) public {
+        Company newCompany = Company(CompanyCount, name, url, caddress, 0);
+        companyMapping[CompanyCount++] = newCompany;
+        addressToCompany[caddress] = newCompany;
+    }
+
+    function getCompanyByAddress(address caddress) public returns (Company) {
+        return addressToCompany[caddress];
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,35 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  paths: {
+    artifacts: "./frontend/src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    polygon_mumbai: {
+      url: `https://polished-attentive-film.matic-testnet.discover.quiknode.pro/30b6d013db1d5594d93f664dccea8fca81ca5b56/`,
+      accounts: [
+        "0x0c4b14ac12d6481fc20178e2e030c75dc59a04eb54a8d09b30330101a1b01962",
+      ],
+      allowUnlimitedContractSize: true,
+    },
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "",
+        initialIndex: 0,
+        count: 10,
+      },
+    },
+    mantle: {
+      url: `https://rpc.testnet.mantle.xyz/`,
+      accounts: [
+        "0x0c4b14ac12d6481fc20178e2e030c75dc59a04eb54a8d09b30330101a1b01962",
+      ],
+      chainId: 5001,
+      allowUnlimitedContractSize: true,
+    },
+  },
 };
