6f3e24a782a771f72ad92844e8093d5db6ce3b44
sahilkedare <sahilkedare4444@gmail.comm>
2023-12-10 07:04:33 +0530

    added changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import ReactDOM from "react-dom/client";
 import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import Generate from "./pages/Generate.jsx";
 import { CouponContextProvider } from "./context/CouponContext.jsx";
-import { AuthContextProvider } from "./context/AuthContext.jsx";
+
 import CompanyLogin from "./pages/CompanyLogin.jsx";
 import CompanyDashboard from "./pages/CompanyDashboard.jsx";
 import UserDashboard from "./pages/UserDashboard.jsx";
-import Generate from "./pages/Generate.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { init } from "@airstack/airstack-react";
+
+init("1f658796ad3724726bc36250b6dd8daf5");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <AuthContextProvider>
@@ -15,12 +19,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <Router>
                 <Routes>
                     <Route path="/" element={<App />} />
+                    <Route path="/generate" element={<Generate />} />
                     <Route path="/companyLogin" element={<CompanyLogin />} />
                     <Route path="/company" element={<CompanyDashboard />} />
                     <Route path="/dashboard" element={<UserDashboard/>} />
-                    <Route path="/generate" element={<Generate />} />
+                    
                 </Routes>
             </Router>
         </CouponContextProvider>
-    </AuthContextProvider>
+        </AuthContextProvider>
 );
